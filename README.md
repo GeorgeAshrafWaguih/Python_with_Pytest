fe223b4c5e61a1296aa815afbb4bbca115bce363
George <George.ashraf@hotmail.com>
2022-12-26 01:58:50 +0200

    Adding failing test

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Python_with_Pytest
 - First test case (test_math.py)
+- Failing test case (test_math.py)
--- venv/test/test_math.py
+++ venv/test/test_math.py
@@ -7,3 +7,11 @@ The purpose is to show how to use the pytest framework by example
 # Basic test function
 def test_one_plus_one():
     assert 1 + 1 == 2
+
+
+# Failed test
+def test_one_plus_two():
+    a = 1
+    b = 2
+    c = 0
+    assert a + b == c
